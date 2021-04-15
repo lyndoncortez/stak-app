18a2dfba3dc473347f7e414f2df1223bf758205e
lyndoncortez <lyndoncortez@yahoo.com>
2021-04-16 01:03:13 +0800

    Adds user spec

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,9 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  context 'validation test' do
+  describe 'validation test' do
     it 'is invalid without a name' do
-      user = User.new(email:'sample@email.com', password: '123456' ).save
+      user = described_class.new(email: 'sample@email.com', password: '123456').save
       expect(user).to eq(false)
     end
   end
