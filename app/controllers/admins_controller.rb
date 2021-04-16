2a44a3782e08720b209b8ecb869eceba4636639b
lyndoncortez <lyndoncortez@yahoo.com>
2021-04-17 02:32:19 +0800

    [WIP]Sets up Landing page and Admin views

--- app/controllers/admins_controller.rb
+++ app/controllers/admins_controller.rb
@@ -1,5 +1,4 @@
 class AdminsController < ApplicationController
   before_action :authenticate_user!
   before_action :is_admin?
-
 end
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::Base
-  devise_group :user, contains: [:user, :admin]
+  devise_group :user, contains: %i[user admin]
   protect_from_forgery with: :exception
   before_action :configure_permitted_parameters, if: :devise_controller?
 
@@ -8,5 +8,5 @@ class ApplicationController < ActionController::Base
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:name, :email, :password, :type) }
     devise_parameter_sanitizer.permit(:account_update) { |u| u.permit(:name, :email, :password, :current_password) }
-  end 
+  end
 end
