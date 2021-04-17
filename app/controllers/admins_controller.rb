25996f4581783663ae5b226896d6e204ae47e095
lyndoncortez <lyndoncortez@yahoo.com>
2021-04-18 00:19:37 +0800

    [WIP] Layout of admin page

--- app/controllers/admins_controller.rb
+++ app/controllers/admins_controller.rb
@@ -2,7 +2,9 @@ class AdminsController < ApplicationController
   before_action :authenticate_user!
   before_action :is_admin?
 
-  def dashboard; end
+  def dashboard 
+    @pending = User.where(approved:false)
+  end
 
   def index; end
 end
