6948135bb33485b8b25f51b1945635e8bb9ec81f
lyndoncortez <lyndoncortez@yahoo.com>
2021-04-19 00:15:39 +0800

    Adds routes for admin

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,10 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   root to: "home#index"
+
+  get 'admins/index'
+  get 'admins/new/user'  => 'admins#new_user',   as: 'admins_new_user'
+  post 'admins/new/user' => 'admins#create_user', as: 'admins_create_user'
+  get 'admins/view/user/:id' => 'admins#view_user', as: 'admins_view_user'
+  get 'admins/pending' => 'admins#pending', as: 'admins_pending'
 end
