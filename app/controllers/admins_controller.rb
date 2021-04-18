ad0cf95c227e50d4a970c94ece84fdc999e473e2
lyndoncortez <lyndoncortez@yahoo.com>
2021-04-19 00:39:40 +0800

    Adds additional views for admin

--- app/controllers/admins_controller.rb
+++ app/controllers/admins_controller.rb
@@ -1,10 +1,22 @@
 class AdminsController < ApplicationController
   before_action :authenticate_user!
-  before_action :is_admin?
+  before_action :admin?
 
-  def dashboard
-    @pending = User.where(approved: false)
-  end
+  def dashboard; end
 
   def index; end
+
+  def new_user
+    @user = User.new
+  end
+
+  def pending
+    @users = User.where(approved: false)
+  end
+
+  private
+
+  def admin?
+    redirect_to root_path unless current_user.type == 'Admin'
+  end
 end
--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,3 +1,7 @@
 class HomeController < ApplicationController
-  def index; end
+  def index
+    @admins_pending = User.where(type: 'Admin', approved: false)
+    @brokers_pending = User.where(type: 'Broker', approved: false)
+    @buyers_pending = User.where(type: 'Buyer', approved: false)
+  end
 end
