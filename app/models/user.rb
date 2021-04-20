ff2ef595280e7d6629a0e5f5b9a9d7f1d56671b9
lyndoncortez <lyndoncortez@yahoo.com>
2021-04-21 01:48:12 +0800

    Adds :type validation to user

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   validates :name, presence: true
+  validates_presence_of :type, :unless => Proc.new {|user| user.type == "Buyer"}
 
   def active_for_authentication?
     super && approved?
