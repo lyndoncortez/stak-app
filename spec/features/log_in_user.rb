0961adf5e5322de2d1f7fd0ffa0b93062990e9c5
lyndoncortez <lyndoncortez@yahoo.com>
2021-04-26 23:29:05 +0800

    Adds login test for admin and broker using capybara & rspec

--- spec/features/log_in_user.rb
+++ spec/features/log_in_user.rb
@@ -4,13 +4,32 @@ RSpec.describe 'Log in user', type: :system do
   describe 'log in as admin' do
     let(:user) { create :user }
 
+    before do
+      user.confirm
+    end
+
     it 'signs the admin in' do
       visit new_admin_session_path
       fill_in 'Email', with: user.email
       fill_in 'Password', with: user.password
       click_button 'Log in'
+      expect(page).to have_current_path(root_path)
+    end
+  end
+
+  describe 'log in as broker' do
+    let(:broker) { create :broker }
+
+    before do
+      broker.confirm
     end
 
-    expect(page).to have_current_path(root_path)
+    it 'signs the broker in' do
+      visit new_broker_session_path
+      fill_in 'Email', with: broker.email
+      fill_in 'Password', with: broker.password
+      click_button 'Log in'
+      expect(page).to have_current_path(home_broker_portfolio_path)
+    end
   end
 end
