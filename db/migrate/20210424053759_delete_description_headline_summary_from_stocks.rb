1dfac608b84edaaeba527ec1a24ee594dff089ef
lyndoncortez <lyndoncortez@yahoo.com>
2021-05-02 23:46:02 +0800

    Deletes a migration file

--- db/migrate/20210424053759_delete_description_headline_summary_from_stocks.rb
+++
@@ -1,7 +0,0 @@
-class DeleteDescriptionHeadlineSummaryFromStocks < ActiveRecord::Migration[6.1]
-  def change
-    remove_column :stocks, :description, :string
-    remove_column :stocks, :headline, :string
-    remove_column :stocks, :summary, :string
-  end
-end
