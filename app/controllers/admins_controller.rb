6963232a9aa2b62a9374f4100e73e0c19f92e0ae
lyndoncortez <lyndoncortez@yahoo.com>
2021-05-03 19:53:23 +0800

    Fixes admin mailer when approving pending user

--- app/controllers/admins_controller.rb
+++ app/controllers/admins_controller.rb
@@ -50,7 +50,7 @@ class AdminsController < ApplicationController
     @email = user.email
     if user.save
       flash[:success] = "#{user.name} approved."
-      AdminMailer.account_approval(@email).deliver_now
+      AdminMailer.account_approved(@email).deliver_now
     else
       flash[:danger] = 'User approval failed.'
     end
--- spec/features/buyer/buy_stocks.rb
+++ spec/features/buyer/buy_stocks.rb
@@ -2,9 +2,23 @@ require 'rails_helper'
 
 RSpec.describe 'Buy Stocks', type: :system do
   let(:buyer) { create :buyer }
+  let(:broker) { create :broker }
 
   before do
     buyer.confirm
+    broker.confirm
+  end
+
+  it 'broker adds a stock' do
+    visit new_broker_session_path
+    fill_in 'Email', with: broker.email
+    fill_in 'Password', with: broker.password
+    click_button 'Log in'
+    fill_in 'symbol', with: 'AAPL'
+    find('#searchId').click
+    sleep(3)
+    find('#addStockId').click
+    expect(page).to have_content('Stock Added')
   end
 
   it 'buys a stock' do
